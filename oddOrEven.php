510fc0e782b862e128d40ac0418870b424a5238e
amircolgang <parham38385747@gmail.com>
2025-06-17 15:23:10 +0330

    Amircolgang: changes on 2025-06-17 15:23:10

--- oddOrEven.php
+++ oddOrEven.php
@@ -1,1 +1,17 @@
-<>
+<?php
+    $number 
+    $isEven 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odd Or Even</title>
+</head>
+<body>
+    <form action="#" onclick=(e) => ></form>
+</body>
+</html>
