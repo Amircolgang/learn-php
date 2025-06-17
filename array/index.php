3d8d6c30f66799ede54582f903bacb05cca23f62
amircolgang <parham38385747@gmail.com>
2025-06-17 16:00:07 +0330

    Amircolgang: changes on 2025-06-17 16:00:06

--- array/index.php
+++ array/index.php
@@ -2,7 +2,7 @@
     $x = array(5,12,8,9);
     // var_dump($x) ;
     foreach($x as $index => $item){
-        echo $index."-".$item;
+        echo "index =>".$index."- item => ".$item;
         echo "<br>";
     };
 ?>
