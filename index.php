2be273e9a7c0cf331d208fdba74484d3e64924cc
amircolgang <parham38385747@gmail.com>
2025-06-17 15:01:10 +0330

    Amircolgang: changes on 2025-06-17 15:01:09

--- index.php
+++ index.php
@@ -15,6 +15,23 @@
 // 
     if($_POST["product"]){
         $result  = $_POST["count"] * $_POST["price"];
-        echo "<span>".$result."</span>" ; 
+        // echo "<span>".$result."</span>" ; 
     };
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <span>
+        <?php 
+            echo $result
+        ?>
+    </span>
+    s
+</body>
+</html>
