2a55a288e2224a3799aeff1c3ecbd17626262eba
amircolgang <parham38385747@gmail.com>
2025-06-17 15:18:17 +0330

    Amircolgang: changes on 2025-06-17 15:18:17

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    // echo "ho" ;
+// echo "ho" ;
 
 //     $number = 100000 ;
 //     printf("this Number : %x",$number ) ;
@@ -13,61 +13,63 @@
 //     echo '</br>' ;
 //     printf("this Number : %f",$number ) ;
 // 
-    if($_POST["name"]){
-        $price = 0 ;
-        switch ($_POST["name"]) {
-            case 'c#':
-                    $price = 20;
-                break;
-            case 'js':
-                    $price = 90;
-                break;
-            
-            case 'react':
-                    $price = 1200;
-                break;
-            
-            default:
-                echo "Not Valid" ;
-                break;
-        }
-        $result  = $_POST["count"] * $price;
-        $name = $_POST["name"];
-        // echo "<span>".$result."</span>" ; 
-    };
+if ($_POST["name"]) {
+    $price = 0;
+    switch ($_POST["name"]) {
+        case 'c#':
+            $price = 20;
+            break;
+        case 'js':
+            $price = 90;
+            break;
+
+        case 'react':
+            $price = 1200;
+            break;
+
+        default:
+            echo "Not Valid";
+            break;
+    }
+    $result  = $_POST["count"] * $price;
+    $name = $_POST["name"];
+    // echo "<span>".$result."</span>" ; 
+};
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Shoma Dar In Doreye 
+    <h1>Shoma Dar In Doreye
         <span>
             Name : <<
-        <?php 
+                <?php
                 echo $name;
-            ?>
-        </span>
-        >>
-        Sabte Nam Kardid
+                ?>
+                </span>
+                >>
+                Sabte Nam Kardid
     </h1>
     <br>
     <span>
-        Mablagh Shoma : 
-    <?php 
-            echo $result;
+        Mablagh Shoma :
+        <?php
+        echo $result;
         ?>
     </span>
     <br>
     <button>
-        Click
-        <?php
-            header("")
-        ?>
+        <a href="store.php">
+            Click
+        </a>
     </button>
 </body>
+
 </html>
